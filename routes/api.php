a5c3c847f387dde9a0057f2fbe2519c4cf0facc3
Víctor Gutiérrez <vgutierrezc.90@gmail.com>
2019-09-12 00:21:59 -0500

    removed unnecesary code

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,6 @@ Route::post('login', 'v1\AuthController@login');
 Route::post('users/forgot_password', 'v1\UserController@forgotPassword');
 Route::post('users/reset_password', 'v1\UserController@resetPassword');
 
-Route::group(['middleware' => 'auth:api'], function() {
-	Route::get('/user', function() {
-		dd('hola');
-	});
-});
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });

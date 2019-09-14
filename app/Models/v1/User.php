93860cb91e80847221ff0e1995592c3451a26320
Víctor Gutiérrez <victor.gutierrez@vetta.io>
2019-09-13 20:29:30 -0500

    init with permissions

--- app/Models/v1/User.php
+++ app/Models/v1/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasApiTokens, SoftDeletes;
+    use Notifiable, HasApiTokens, SoftDeletes, HasRoles;
 
     /**
      * The attributes that are mass assignable.
